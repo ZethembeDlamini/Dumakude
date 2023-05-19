7c07bb6539d0b1e653a574a68078981ca8c2e41c
ZethembeDlamini <119890298+ZethembeDlamini@users.noreply.github.com>
2023-05-19 17:49:56 +0200

    Initial commit

---
+++ guestHouse/Controllers/roomBooksController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using guestHouse;
+
+namespace guestHouse.Controllers
+{
+    [Authorize]
+    public class roomBooksController : Controller
+    {
+        private dumakudeDBEntities1 db = new dumakudeDBEntities1();
+
+        // GET: roomBooks
+        public ActionResult Index()
+        {
+            var roomBooks = db.roomBooks.Include(r => r.Room);
+            return View(roomBooks.ToList());
+        }
+
+        // GET: roomBooks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            roomBook roomBook = db.roomBooks.Find(id);
+            if (roomBook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(roomBook);
+        }
+
+        // GET: roomBooks/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            ViewBag.RoomID = new SelectList(db.Rooms, "Id", "roomType");
+            return View();
+        }
+
+        // POST: roomBooks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,name,email,phoneNumber,checkIn,checkOut,numberOfPeople,comments,RoomID")] roomBook roomBook)
+        {
+            if (ModelState.IsValid)
+            {
+                db.roomBooks.Add(roomBook);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RoomID = new SelectList(db.Rooms, "Id", "roomType", roomBook.RoomID);
+            return View(roomBook);
+        }
+
+        // GET: roomBooks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            roomBook roomBook = db.roomBooks.Find(id);
+            if (roomBook == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RoomID = new SelectList(db.Rooms, "Id", "roomType", roomBook.RoomID);
+            return View(roomBook);
+        }
+
+        // POST: roomBooks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,name,email,phoneNumber,checkIn,checkOut,numberOfPeople,comments,RoomID")] roomBook roomBook)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(roomBook).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RoomID = new SelectList(db.Rooms, "Id", "roomType", roomBook.RoomID);
+            return View(roomBook);
+        }
+
+        // GET: roomBooks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            roomBook roomBook = db.roomBooks.Find(id);
+            if (roomBook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(roomBook);
+        }
+
+        // POST: roomBooks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            roomBook roomBook = db.roomBooks.Find(id);
+            db.roomBooks.Remove(roomBook);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ guestHouse/Models/Logic.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace guestHouse.Models
+{
+    public class Logic
+    {
+
+       public static bool checkAvailability(int roomNumber,DateTime checkInDate, DateTime checkOutDate) 
+        {
+            using (var db = new dumakudeDBEntities1()) // replace with your database context
+            {
+                var bookings = db.roomBooks.Where(b => b.RoomID == roomNumber).ToList();
+
+                foreach (var booking in bookings)
+                {
+
+                        if (checkInDate < DateTime.Parse(booking.checkIn) && checkOutDate > DateTime.Parse(booking.checkOut))
+                        {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        public class invoice
+        {
+            public string name { get; set; }
+            public int price { get; set; }
+            public string email { get; set; }
+            public string  checkIn { get; set; }
+            public string checkOut { get; set; }
+            public int meals { get; set; }
+
+
+            public  invoice(string name2, string email2, string checkIn2, string checkOut2, int price2, int meals2)
+            {
+                price = price2;
+                name = name2;
+                email = email2;
+                checkIn = checkIn2;
+                checkOut = checkOut2;
+                meals = meals2;
+            }
+        }
+
+    }
+
+    public class AddMeals
+    {
+        [DisplayName("BreakFast")]
+        public bool addBreakFast { get; set; }
+        [DisplayName("Supper")]
+        public bool addSupper { get; set; }
+    }
+}
